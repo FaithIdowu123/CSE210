2041d42dc819f98e719f3e1637a028c17e8c45e0
FaithIdowu123 <ofaith@byupathway.edu>
2025-07-10 03:21:44 +0100

    week2

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,30 @@
-using System;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        fraction1.SetTop(3);
+        fraction1.SetBottom(4);
+
+        fraction2.SetTop(6);
+        fraction2.SetBottom(7);
+
+        fraction3.SetTop(4);
+        fraction3.SetBottom(1);
+
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
     }
 }
