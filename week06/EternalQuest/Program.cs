a51368ba0b3c2309add7b3ccdd238b61cc6c27f2
FaithIdowu123 <ofaith@byupathway.edu>
2025-08-07 19:11:05 +0100

    change

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,4 +1,4 @@
-// For my exceeding expectations, I added a new variable List<Goal> called _completedGoals and stored the completed goals in it, I also saved the completed goals in a txt file and created new methods for saving and loading it.
+// For my exceeding expectations, I added a new variable List<Goal> called _completedGoals, a new method called GetCompleted to the goal manager class and I also created a getcomplete method to the checklist and simple goal classes in order to check if the goal has been completed. I stored the completed goals in the new completedgoals list, saved the completed goals in a txt file and created new methods for saving and loading it.
 // I also made it so that completed goals will only appear the list completed goals option and no longer appear in the list goals option.
 using System;
 
