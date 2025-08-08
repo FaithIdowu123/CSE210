2fbc206d091a6720a24b292e9d98d2b1f6e7be5d
FaithIdowu123 <ofaith@byupathway.edu>
2025-08-09 00:46:22 +0100

    change

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,4 +1,5 @@
-// For my exceeding expectations, i made it that whenever the user gets 20 points, their level increases by one. 
+// For my exceeding expectations, I made it so that whenever the user gets 20 points, their level increases by one. 
+// I also made it that the user will not be able to get points for goals that have already been completed. except for eternal goals.
 using System;
 
 class Program
